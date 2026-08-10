c8a00c2c4391c17feda7501aab3f1f369ed8656d
Pavithra Sharma <pavithrasharma456@gmail.com>
2026-08-10 09:36:16 +0530

    Time: 390 ms (37.25%) | Memory: 0B (100.00%) - LeetSync

---
+++ 626-exchange-seats/exchange-seats.sql
@@ -0,0 +1,19 @@
+# Write your MySQL query statement below
+SELECT 
+    s1.id,
+    CASE
+        WHEN s1.id % 2 = 1 AND s1.id = (SELECT MAX(id) FROM Seat)
+            THEN s1.student
+        ELSE s2.student
+    END AS student
+FROM Seat s1
+LEFT JOIN Seat s2
+    ON (
+        s1.id % 2 = 1
+        AND s2.id = s1.id + 1
+    )
+    OR (
+        s1.id % 2 = 0
+        AND s2.id = s1.id - 1
+    )
+ORDER BY s1.id;
